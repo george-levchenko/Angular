d7642fd6adedd007f63e7b32a185ccdd5b2a9dfd
George <Saidon10@ukr.net>
2019-06-25 16:28:40 +0300

    Upgrading to angular8 (v2)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'components',
-    loadChildren: './practice-components/components.module#ComponentsModule' },
+    loadChildren: () => import('./practice-components/components.module').then(m => m.ComponentsModule) },
   { path: 'components-relation',
-    loadChildren: './practice-components-relation/components-relation.module#ComponentsRelationModule' },
+    loadChildren: () => import('./practice-components-relation/components-relation.module').then(m => m.ComponentsRelationModule) },
   { path: 'directives-pipes',
-    loadChildren: './practice-directives-pipes/directives-pipes.module#DirectivesPipesModule' },
+    loadChildren: () => import('./practice-directives-pipes/directives-pipes.module').then(m => m.DirectivesPipesModule) },
   { path: 'http-routing',
-    loadChildren: './practice-http-routing/http-routing.module#HttpRoutingModule' },
+    loadChildren: () => import('./practice-http-routing/http-routing.module').then(m => m.HttpRoutingModule) },
   { path: 'observables',
-    loadChildren: './practice-observables/observables.module#ObservablesModule' }
+    loadChildren: () => import('./practice-observables/observables.module').then(m => m.ObservablesModule) }
 ];
 
 @NgModule({
